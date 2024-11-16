b051115802956824481ae35fee2fe7f7800ff266
Yu, Lexun <lyu@prpr.one>
2024-11-16 13:44:56 -0500

    update weather data

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,26 +1,18 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Statistics Canada
+# Author: Lexun Yu
+# Date: 12 November 2024
+# Contact: lx.yu@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+url <- "https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=5415&Year=2013&Month=1&Day=1&time=&timeframe=3&submit=Download+Data"
 
-
+ahccd_url <- "https://api.weather.gc.ca/collections/ahccd-annual/items?datetime=1840/2020&station_id__id_station=7025250&sortby=province__province,identifier__identifiant&f=csv&limit=10000&offset=0"
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
-         
+download.file(url, "data/01-raw_data/climateyul.csv")
+download.file(ahccd_url, "data/01-raw_data/ahccdyul.csv")
