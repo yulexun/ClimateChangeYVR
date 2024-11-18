183a6c794af621dbd4c74730eb1cad5dfffe2d3f
Lexun Yu <lx.yu@mail.utoronto.ca>
2024-11-18 15:55:18 -0500

    update style

--- scripts/05-exploratory_data_analysis.R
+++ scripts/05-exploratory_data_analysis.R
@@ -19,24 +19,24 @@ analysis_data <- read_parquet("data/02-analysis_data/cleaned_data.parquet")
 #### Plot ####
 attach(analysis_data)
 summary(analysis_data)
-hist(mean_temp, breaks=100, main="Mean Temperature", col="red")
-hist(log(mean_temp), breaks=100, main="Mean Temperature", col="red")
+hist(mean_temp, breaks = 100, main = "Mean Temperature", col = "red")
+hist(log(mean_temp), breaks = 100, main = "Mean Temperature", col = "red")
 
 par(mfrow = c(3, 3), mar = c(4, 4, 2, 1))
-hist(wind_speed, breaks=100, main="Wind Speed")
-hist(total_precipitation, breaks=100, main="Total Precipitation")
-hist(snow, breaks=100, main="total snow")
-hist(pressure_station, breaks=100, main="Pressure")
-hist(max_temp, breaks=100, main="Max Temperature")
-hist(min_temp, breaks=100, main="Min Temperature")
-hist(mean_temp, breaks=100, main="Mean Temperature")
-hist(total_rain, breaks=100, main="Rain")
-hist(gust_speed_km_h, breaks=100, main="Max Gust Speed")
+hist(wind_speed, breaks = 100, main = "Wind Speed")
+hist(total_precipitation, breaks = 100, main = "Total Precipitation")
+hist(snow, breaks = 100, main = "total snow")
+hist(pressure_station, breaks = 100, main = "Pressure")
+hist(max_temp, breaks = 100, main = "Max Temperature")
+hist(min_temp, breaks = 100, main = "Min Temperature")
+hist(mean_temp, breaks = 100, main = "Mean Temperature")
+hist(total_rain, breaks = 100, main = "Rain")
+hist(gust_speed_km_h, breaks = 100, main = "Max Gust Speed")
 
 
 
 # Set layout for multiple plots
-par(mfrow = c(3, 3), mar = c(4, 4, 2, 1))  # Adjust margins for better fit
+par(mfrow = c(3, 3), mar = c(4, 4, 2, 1)) # Adjust margins for better fit
 
 # Log-transformed histograms
 hist(log(analysis_data$wind_speed), breaks = 100, main = "Log(Wind Speed)", xlab = "Log(Wind Speed)", col = "blue")
@@ -47,7 +47,7 @@ hist(log(analysis_data$max_temp + abs(min(analysis_data$max_temp)) + 1), breaks 
 hist(log(abs(analysis_data$min_temp) + 1), breaks = 100, main = "Log(Min Temperature)", xlab = "Log(Abs(Min Temp) + 1)", col = "cyan")
 hist(log(analysis_data$mean_temp + abs(min(analysis_data$mean_temp)) + 1), breaks = 100, main = "Log(Mean Temperature)", xlab = "Log(Mean Temp Adjusted)", col = "pink")
 hist(log(analysis_data$total_rain + 1), breaks = 100, main = "Log(Total Rain)", xlab = "Log(Total Rain + 1)", col = "yellow")
-hist(log(gust_speed_km_h), breaks=100, main="Log(Max Gust Speed)")
+hist(log(gust_speed_km_h), breaks = 100, main = "Log(Max Gust Speed)")
 
 pairs(analysis_data[, 2:8], main = "Pairwise Scatterplots (Columns 1-8)")
 
@@ -59,4 +59,3 @@ pairs(as.data.frame(selected_data), main = "Pairwise Scatterplots (Excluding Sno
 
 
 #### Save model ####
-
--- scripts/06-model_data.R
+++ scripts/06-model_data.R
@@ -15,7 +15,7 @@ library(brms)
 library(car)
 library(modelsummary)
 library(boot)
-
+library(arrow)
 
 #### Read data ####
 analysis_data <- read_parquet("data/02-analysis_data/cleaned_data.parquet")
@@ -25,27 +25,31 @@ analysis_data <- read_parquet("data/02-analysis_data/cleaned_data.parquet")
 # MLR
 
 
-m1 <- lm(mean_temp ~ wind_speed + total_precipitation + snow + 
-  pressure_station + max_temp + min_temp + total_rain + gust_speed_km_h, data=analysis_data)
+m1 <- lm(mean_temp ~ wind_speed + total_precipitation + snow +
+  pressure_station + max_temp + min_temp + total_rain + gust_speed_km_h, data = analysis_data)
 summary(m1)
 
-m2 <- lm(mean_temp ~ wind_speed + total_precipitation + snow + 
-  pressure_station + total_rain + gust_speed_km_h, data=analysis_data)
+m2 <- lm(mean_temp ~ wind_speed + total_precipitation + snow +
+  pressure_station + total_rain + gust_speed_km_h, data = analysis_data)
 summary(m2)
 
 m3 <- lm(mean_temp ~ wind_speed + total_precipitation +
-  pressure_station + total_rain + gust_speed_km_h, data=analysis_data)
+  pressure_station + total_rain + gust_speed_km_h, data = analysis_data)
 summary(m3)
 AIC(m3)
 
 # Bayesian Model
-bayesian_model <- brm(mean_temp ~ wind_speed + total_precipitation + pressure_station +
-  total_rain + gust_speed_km_h,
+bayesian_model <- brm(
+  mean_temp ~ wind_speed + total_precipitation + pressure_station +
+    total_rain + gust_speed_km_h,
   data = analysis_data,
   family = gaussian(),
-  prior = c(prior(normal(0, 10), class = "b"),
-            prior(normal(0, 10), class = "Intercept")),
-  chains = 4, iter = 2000, cores = 4)
+  prior = c(
+    prior(normal(0, 10), class = "b"),
+    prior(normal(0, 10), class = "Intercept")
+  ),
+  chains = 4, iter = 2000, cores = 4
+)
 
 # Summary of results
 summary(bayesian_model)
@@ -57,12 +61,12 @@ analysis_data$log_mean_temp <- log(analysis_data$mean_temp + 3)
 
 
 bayesian_model_log <- brm(
-  formula = log_mean_temp ~ wind_speed + total_precipitation + pressure_station + 
-             total_rain + gust_speed_km_h,
+  formula = log_mean_temp ~ wind_speed + total_precipitation + pressure_station +
+    total_rain + gust_speed_km_h,
   data = analysis_data,
   family = gaussian(),
   prior = c(
-    prior(normal(0, 10), class = "b"),       # Priors for coefficients
+    prior(normal(0, 10), class = "b"), # Priors for coefficients
     prior(normal(0, 10), class = "Intercept") # Prior for the intercept
   ),
   chains = 4, iter = 2000, cores = 4
@@ -75,18 +79,22 @@ summary(bayesian_model_log)
 pp_check(bayesian_model_log)
 
 # GLM
-glm_model <- glm(mean_temp ~ wind_speed + total_precipitation + pressure_station + 
-  total_rain + gust_speed_km_h,
+glm_model <- glm(
+  mean_temp ~ wind_speed + total_precipitation + pressure_station +
+    total_rain + gust_speed_km_h,
   data = analysis_data,
-  family = gaussian())
+  family = gaussian()
+)
 
 summary(glm_model)
 modelsummary(glm_model)
 
-glm_model_log <- glm(log(mean_temp) ~ wind_speed + total_precipitation + pressure_station +
-                 total_rain + gust_speed_km_h,
-                 data = analysis_data,
-                 family = gaussian())
+glm_model_log <- glm(
+  log(mean_temp) ~ wind_speed + total_precipitation + pressure_station +
+    total_rain + gust_speed_km_h,
+  data = analysis_data,
+  family = gaussian()
+)
 
 summary(glm_model_log)
 modelsummary(glm_model_log)
@@ -95,7 +103,7 @@ modelsummary(glm_model_log)
 cv_glm <- cv.glm(data = analysis_data, glmfit = glm_model_log, K = 10)
 
 # RMSE for GLM
-rmse_glm <- sqrt(cv_glm$delta[1])  # Extract cross-validation error
+rmse_glm <- sqrt(cv_glm$delta[1]) # Extract cross-validation error
 print(rmse_glm)
 
 
